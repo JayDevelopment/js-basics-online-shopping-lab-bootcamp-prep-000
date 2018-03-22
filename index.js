f4a9b6fe4bd9d0fd3e6cca787d23d3189fe541d3
Jordan Merrill <amanithought@gmail.com>
2018-03-22 22:05:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function total() {
 
 function removeFromCart(item) {
   for (var i = 0; i <cart.length; i++) {
-    if (cart[i].item) {
+    if (cart[i].itemName) {
       cart.splice(i, 1)
       return cart
     } 
